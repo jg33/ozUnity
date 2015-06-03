b632c4d63c47e05d0159486833662cbf2b83ff09
Jesse Garrison <jesse@takethefort.com>
2015-06-03 17:47:24 -0400

    fixing arm7 vuforia issue

--- Assets/Plugins/iOS/QCARUnityPlayer.h
+++ Assets/Plugins/iOS/QCARUnityPlayer.h
@@ -5,36 +5,16 @@ Qualcomm Confidential and Proprietary
 ============================================================================*/
 
 
-class QCARUnityPlayer
+#ifdef __cplusplus
+extern "C"
 {
-public:
-    static QCARUnityPlayer& getInstance();
-    static void destroy();
+#endif
+   
+   int getRotationFlag(int screenOrientation);
+   void setPlatFormNative();
+   int initQCARiOS(int ScreenOrientation, const char* licenseKey);
+   void setSurfaceOrientationiOS(int orientation);
     
-    void testLog();
-    
-    void QCARInit(const char* uiScreenOrientation);
-    void QCARPause(bool pause);
-    
-    // loads the QCAR trackers
-    void QCARLoadTracker();
-    
-    // notify QCAR that the rendering surface has been created
-    // with given width and height
-    void QCARNotifyCreated(int width, int height);
-
-    void QCARSetOrientation(int orientation);
-
-    
-protected:
-    QCARUnityPlayer();
-    virtual ~QCARUnityPlayer();
-    
-    // deinitialises QCAR and stops logging
-    void QCARCleanup();
-    
-    int getRotationFlag(const char* uiScreenOrientation);
-    
-    static QCARUnityPlayer* instance;
-};
-
+#ifdef __cplusplus
+}
+#endif
