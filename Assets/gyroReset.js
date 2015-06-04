303635e1f1c694d67c2041c3d3b04803a16bbbfb
Jesse Garrison <jesse@takethefort.com>
2015-06-04 13:15:24 -0400

    new gyro resetter, ar test scene

---
+++ Assets/gyroReset.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+public function resetGyro(){
+    		var  invertedOrientation:Quaternion;
+    		invertedOrientation = Quaternion.Inverse(transform.localRotation);
+    		var invertedEulers = invertedOrientation.eulerAngles;
+    		invertedOrientation = Quaternion.Euler(invertedEulers.x, invertedEulers.y, invertedEulers.z   );
+    		transform.localRotation = invertedOrientation;
+
+   		 }
