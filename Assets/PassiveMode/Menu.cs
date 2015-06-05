4642db82a2a25a8f1980be3141750471d8c6bc4a
kdloney <kdloney@gmail.com>
2015-06-04 21:11:51 -0400

    PassiveModeGui_SepreateScene

---
+++ Assets/PassiveMode/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+public class Menu : MonoBehaviour {
+
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool IsOpen
+	{
+		get {return _animator.GetBool ("IsOpen"); }
+		set { _animator.SetBool ("IsOpen", value);}
+	}
+
+
+	public void Awake()
+	{
+
+
+		_animator = GetComponent<Animator>();
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0,0);
+	}
+
+	public void Update()
+	{
+		if (_animator.GetCurrentAnimatorStateInfo(0).IsName("OzInfo_Idle"))
+		{
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+
+		}
+		else
+		{
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+
+		}
+	}
+}
