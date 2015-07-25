961b732e7384a418e6a8527ea4aec120051f63f1
Jesse Garrison <jesse@takethefort.com>
2015-07-25 16:20:49 -0700

    new UI, prepped state saving between app launches

---
+++ Assets/ClientStateMachine.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+function Start () {
+	
+	if(PlayerPrefs.GetInt("FirstLaunch",0) != 0){
+	
+		GameObject.Find("Intro").SetActive(false);
+	
+	}
+
+	if (PlayerPrefs.GetInt("CompletedShow",0) != 0){
+		
+		//GameObject.Find("Extras Button").GetComponent("Image").color.a = 255;
+	
+	}
+	
+	PlayerPrefs.SetInt("FirstLaunch", 1);
+	PlayerPrefs.Save();
+	
+}
+
+function Update () {
+
+
+	if(Input.GetKey('d')) {
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+	
+	}
+	
+	if (Input.GetKey('c') ){
+	
+		setCompletedShow();
+		PlayerPrefs.Save();
+	
+	}
+	
+}
+
+
+function setCompletedShow(){
+	
+	PlayerPrefs.SetInt("CompletedShow",1);
+	PlayerPrefs.Save();
+
+}
--- Assets/panelCounter.js
+++ Assets/panelCounter.js
@@ -4,6 +4,8 @@
 var currentPanel:int;
 var animator:Animator;
 
+private var totalPanels = 4;
+
 function Start () {
 	currentPanel = 1;
 }
@@ -15,9 +17,12 @@ function Update () {
 function panelUp(){
 	currentPanel++;
 	animator.SetInteger("panel #", currentPanel);
+	if (currentPanel > totalPanels) currentPanel = 1;
 }
 
 function panelDown(){
 	currentPanel--;
 	animator.SetInteger("panel #", currentPanel);
+	if (currentPanel < 1) currentPanel = 1;
+
 }
