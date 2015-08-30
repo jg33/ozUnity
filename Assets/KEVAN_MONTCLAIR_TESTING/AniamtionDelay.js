3f9915917ae76789bce36a4add6efd38dba26eeb
kdloney <kdloney@gmail.com>
2015-08-29 20:35:28 -0400

    Working On Poppies - This is out side the master client in a separate scene.

---
+++ Assets/KEVAN_MONTCLAIR_TESTING/AniamtionDelay.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var startDelay : int;
+  
+function Start(){ 
+      WaitSeconds(); //wait random seconds for animation
+}
+
+
+function WaitSeconds () {
+    while(true) {
+           yield WaitForSeconds(startDelay);
+           GetComponent.<Animator>().Play("Take 001");
+		  // GetComponent.<Animator>().Play("PoppyGrowing_Petals");    
+		  }         
+}
