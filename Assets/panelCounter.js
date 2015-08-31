47311afad5b11a40ed7a7dc78c42db27154604b6
Jesse Garrison <jesse@takethefort.com>
2015-08-31 10:56:40 -0400

    close / done buttons

--- Assets/panelCounter.js
+++ Assets/panelCounter.js
@@ -4,10 +4,13 @@
 var currentPanel:int;
 var animator:Animator;
 
+private var backButton:GameObject;
+private var nextButton:GameObject;
 private var totalPanels = 4;
 
 function Start () {
 	currentPanel = 0;
+	
 }
 
 function Update () {
@@ -18,11 +21,36 @@ function panelUp(){
 	currentPanel++;
 	if (currentPanel > totalPanels) currentPanel = 0;
 	animator.SetInteger("panel #", currentPanel);
+	
+	
+	setButtons();
+
 }
 
 function panelDown(){
 	currentPanel--;
 	if (currentPanel < 1) currentPanel = 0;
 	animator.SetInteger("panel #", currentPanel);
+	
+	setButtons();
+}
 
+function setButtons(){
+	/// Button ///
+	backButton = GameObject.Find("InstructionBackText");
+	nextButton = GameObject.Find("InstructionNextText");
+
+	if(currentPanel == 1){
+		backButton.GetComponent(UI.Text).text = "CLOSE";
+		nextButton.GetComponent(UI.Text).text = "NEXT";
+
+		} else if(currentPanel == 4){
+		nextButton.GetComponent(UI.Text).text = "DONE";
+		backButton.GetComponent(UI.Text).text = "BACK";
+
+		} else{
+		backButton.GetComponent(UI.Text).text = "BACK";
+		nextButton.GetComponent(UI.Text).text = "NEXT";
+
+		}
 }
