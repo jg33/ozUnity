7b134ab52d59fd5c0a1848003d5f5c3d934b47c3
kdloney <kdloney@gmail.com>
2015-09-04 12:10:51 -0400

    FIRE!

---
+++ Assets/Detailed_Pyro_FX/Scripts/LoadLevel_Fire.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel_Fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel("Smoke");
+	}
+	
+	
+	
+	public void OnGUI()
+	{
+		if (GUI.Button(new Rect(70, 70, 100, 30), "Next Effects"))
+			Application.LoadLevel("Smoke");
+	}
+	
+	
+	
+}
