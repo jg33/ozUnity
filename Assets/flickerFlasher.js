9d6775b4538313c838bd73c8d180b3eec0161f46
Jesse Garrison <jesse@takethefort.com>
2015-09-14 23:33:38 -0400

    flashaaaaa

---
+++ Assets/flickerFlasher.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var flasher:UI.Image;
+
+
+function Start () {
+	flasher= this.gameObject.GetComponent(UI.Image);
+
+}
+
+function Update () {
+	flasher.color.a = Random.Range(0,1000)*0.001;
+	
+}
