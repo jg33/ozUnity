c7233133c0c70bd6b957badf8fcfdf3d4351ebee
Jesse Garrison <jesse@takethefort.com>
2015-09-16 12:25:26 -0400

    first pass at logo masking

---
+++ Assets/vuforiaMasker.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+public var tornadoScene:GameObject;
+public var munchkinScene:GameObject;
+public var poppyScene:GameObject;
+public var fireScene:GameObject;
+public var monkeyScene:GameObject;
+
+
+private var renderComponent:Canvas;
+
+
+function Start () {
+	renderComponent  = this.gameObject.GetComponent(Canvas);
+}
+
+function Update () {
+
+	if(tornadoScene.active || munchkinScene.active || poppyScene.active || fireScene.active || monkeyScene.active){
+		renderComponent.enabled = true;
+		renderComponent.sortingOrder = -1;
+
+	} else {
+		renderComponent.enabled = false;
+	}
+
+}
